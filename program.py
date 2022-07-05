70106a347036a7383595d6a967e18758e36534df
Rajesh G <29516506+dude-with-the-long-username@users.noreply.github.com>
2022-07-06 01:38:41 +0530

    ✨ feat: ocr works and copies to clipboard

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 
 # TODO: fix issue: will fail if latest clipboard entry is not an image
+# TODO: fix: don't create temporary files
 from fileinput import filename
 import subprocess
 from PIL import Image
@@ -10,7 +11,6 @@ command = 'rm -f screenshot_for_ocr_temp_file.png' # deleting file if it exists 
 subprocess.call(command, shell=True)
 
 command = 'xclip -selection clipboard -t image/png -o > screenshot_for_ocr_temp_file.png'  # outputs image in clipboard to screenshot_for_ocr_temp_file.png (if image exists in clipboard)
-# TODO: program will break if file "screenshot_for_ocr_temp_file.png" already exists
 subprocess.call(command, shell=True)
 
 filename = 'screenshot_for_ocr_temp_file.png'
@@ -18,3 +18,14 @@ img1 = np.array(Image.open(filename))
 text = pytesseract.image_to_string(img1)
 # print(text)
 
+with open ('text_obtained_from_ocr.txt', 'w') as output_file:
+    output_file.write(text)
+
+command = 'cat text_obtained_from_ocr.txt | xclip -selection clipboard'
+subprocess.call(command, shell=True)
+
+
+
+# deleting both temporary files
+command = 'rm -f screenshot_for_ocr_temp_file.png text_obtained_from_ocr.txt' # deleting file if it exists (tries even if it doesn't exist)
+subprocess.call(command, shell=True)
